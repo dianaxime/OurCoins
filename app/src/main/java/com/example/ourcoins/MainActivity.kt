f7bc482f117cba94f247c22f05174f929bcbc818
Diana De Leon <dianaximeo@gmail.com>
2019-05-07 18:57:32 -0600

    Diseño1

--- app/src/main/java/com/example/ourcoins/MainActivity.kt
+++ app/src/main/java/com/example/ourcoins/MainActivity.kt
@@ -8,13 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*hola estoy probando esto*/
 
-        //hola que tal jeje salu2
-        //Segunda prueba att. Fatima
-
-        //Ultima prueba de que t o d o funcione
-        //att. Diana
     }
 }
 
