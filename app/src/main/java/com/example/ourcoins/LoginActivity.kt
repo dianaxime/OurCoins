07e96eb76570c93ba6c649d86ae51f16df229155
Diana De Leon <dianaximeo@gmail.com>
2019-06-02 10:48:21 -0600

    Login and Register Modified

--- app/src/main/java/com/example/ourcoins/LoginActivity.kt
+++ app/src/main/java/com/example/ourcoins/LoginActivity.kt
@@ -5,44 +5,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
-import android.widget.ProgressBar
 import android.widget.Toast
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : AppCompatActivity() {
     lateinit var  mAuth: FirebaseAuth
-    lateinit var mProgressbar : ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        mAuth= FirebaseAuth.getInstance()
-    }
-    fun Login(view: View) {
-        val email = Email.text.toString().trim()
-        val password = Password.text.toString().trim()
+        mAuth= FirebaseAuth.getInstance();
+        LoginButton.setOnClickListener {
+            val email = Email.text.toString().trim()
+            val password = Password.text.toString().trim()
 
-        if(TextUtils.isEmpty(email)){
-            Email.error = " Enter Email"
+            if(TextUtils.isEmpty(email)){
+                Email.error = " Enter Email"
+                return@setOnClickListener
+            }
+
+            if(TextUtils.isEmpty(password)){
+                Password.error = " Enter Password"
+                return@setOnClickListener
+            }
+
+            loginUser(email , password)
         }
 
-        if(TextUtils.isEmpty(password)){
-            Email.error = " Enter Password"
+        RegisterButton.setOnClickListener {
+            val registerActivity = Intent(applicationContext , Register::class.java)
+            startActivity(registerActivity)
+            finish()
         }
-
-        loginUser(email , password)
-    }
-
-    fun Register (view: View){
-        val registerActivity = Intent(applicationContext , Register::class.java)
-        startActivity(registerActivity)
-        finish()
     }
     private fun loginUser(email: String, password: String) {
-        mProgressbar.visibility= View.VISIBLE
+        progressBar2.visibility= View.VISIBLE
 
         mAuth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -50,11 +49,11 @@ class LoginActivity : AppCompatActivity() {
                     val startIntent  = Intent(applicationContext , MainActivity::class.java)
                     startActivity(startIntent)
                     finish()
-                    mProgressbar.visibility= View.GONE
+                    progressBar2.visibility= View.GONE
                 } else {
 
-                    Toast.makeText(this, "Authentication failed.${task.exception}", Toast.LENGTH_SHORT).show()
-                    mProgressbar.visibility= View.GONE
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    progressBar2.visibility= View.GONE
                 }
             }
     }
--- app/src/main/java/com/example/ourcoins/Register.kt
+++ app/src/main/java/com/example/ourcoins/Register.kt
@@ -1,26 +1,26 @@
 package com.example.ourcoins
 
-import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
 
-    lateinit var mProgressbar : ProgressDialog
+
     lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         mAuth = FirebaseAuth.getInstance();
-        mProgressbar = ProgressDialog(this)
 
-        RRegisterButton.setOnClickListener {
+
+        RLoginButton.setOnClickListener {
 
             val loginIntent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(loginIntent)
@@ -48,8 +48,8 @@ class Register : AppCompatActivity() {
     }
     fun createUser( email: String, password: String) {
 
-        mProgressbar.setMessage("Please wait..")
-        mProgressbar.show()
+
+        progressBar.visibility= View.VISIBLE
 
 
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -59,12 +59,12 @@ class Register : AppCompatActivity() {
                     val Intent = Intent(applicationContext, MainActivity::class.java)
                     startActivity(Intent)
                     finish()
-                    mProgressbar.dismiss()
+                    progressBar.visibility=View.GONE
 
                 } else {
 
-                    Toast.makeText(this, "Authentication failed.${task.exception}", Toast.LENGTH_SHORT).show()
-                    mProgressbar.dismiss()
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    progressBar.visibility=View.GONE
 
                 }
             }
